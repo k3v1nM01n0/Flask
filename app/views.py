ddfea4fd66344cce3bed92b9414169caa921e20d
root <root@localhost.localdomain>
2019-02-24 17:26:53 +0300

    just learning Flask

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+
+from app import app
+
+app.route('/templates')
+def index():
+    title = 'Welcome to the best Movie Review website'
+    return render_template('index.html',title = title)
+    
+@app.route('/movie/<int:movie_id>')
+def movie(movie_id):
+    return render_template('movie.html',id = movie_id)
+
