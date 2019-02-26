2b7599ea49525a88f616e846e2f074f5a600cf8b
Kevin Moino <kevinmoino10@gmail.com>
2019-02-26 22:35:28 +0300

    properly arranged the project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import DevConfig
 
-app = Flask(__name__,instance_relative_config = True)
+app = Flask(__name__, instance_relative_config = True)
 
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
