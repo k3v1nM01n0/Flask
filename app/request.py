c327435d05a0a4f4e6d5bca6b183fe9058d832b5
Kevin Moino <kevinmoino10@gmail.com>
2019-02-26 11:47:00 +0300

    api

--- app/request.py
+++ app/request.py
@@ -1,3 +1,8 @@
 from app import app
+import urllib.request, json
+from .models import movie
+
+movie = movie.Movie
 
 api_key = app.config['MOVIE_API_KEY']
+base_url = app.config("MOVIE_API_BASE_URL")
